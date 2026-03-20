c969c1b9fc12db5517a24c27b4ce4ecae5224d3c
Satish Wagle <aadesh.wagle@gmail.com>
2026-03-20 01:29:01 -0500

    fix:connect to GCP backend

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://crestmind-api-88776185583.us-central1.run.app' 
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
